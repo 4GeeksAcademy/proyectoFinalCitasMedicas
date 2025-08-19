1fcccf10a05f22302a98dd669b7dcc93c9a7c8d9
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-08-19 20:03:31 +0000

    Navbar2 ok :tada:

--- src/front/pages/InicioHome.jsx
+++ src/front/pages/InicioHome.jsx
@@ -1,10 +1,18 @@
-
+import { Navbar2 } from "../components/Navbar2"
 
 export const InicioHome = () => {
 
     return(
-        <div>
-            <h1>Hola</h1>
+        <div
+        
+        style={{backgroundColor: 'black',
+            margin: 0,
+                paddingTop: '20px',
+                minHeight: '100vh'
+        }}
+        >
+            
+            < Navbar2 />
         </div>
     )
 }
