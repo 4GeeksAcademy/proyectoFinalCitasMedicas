6dc1677f248da0349df24ea0c402216fa5c4353e
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-09-03 19:50:33 +0000

    Frontend register and log in OK :art:

--- src/front/pages/PaginaRegistrate.jsx
+++ src/front/pages/PaginaRegistrate.jsx
@@ -19,47 +19,50 @@ const PaginaRegistrate = () => {
             <h1 className="mt-3">Regístrate</h1>
           </div>
           <form className="needs-validation">
-            <div className="mb-3">
-
+            <div className="mb-3 d-flex">
+              <span className="col-4 input-group-text bg-dark text-white rounded-start-pill" id="addon-wrapping">Nombre</span>
               <input
                 type="text"
-                className="form-control bg-white"
+                className="form-control bg-white rounded-end-pill"
                 id="name"
                 name="name"
-                placeholder="Nombre completo"
+                placeholder="Usuario"
                 required
               />
               <div className="invalid-feedback">Ingresa tu nombre completo.</div>
             </div>
 
-            <div className="mb-3">
+            <div className="mb-3 d-flex">
+              <span className="col-4 input-group-text bg-dark text-white rounded-start-pill" id="addon-wrapping">Teléfono</span>
               <input
                 type="tel"
-                className="form-control bg-white"
+                className="form-control bg-white rounded-end-pill"
                 id="phone"
                 name="phone"
-                placeholder="Teléfono"
+                placeholder="555-5555-5555"
                 required
               />
               <div className="invalid-feedback">Ingresa tu número de teléfono.</div>
             </div>
 
-            <div className="mb-3">
+            <div className="mb-3 d-flex">
+              <span className="col-4 input-group-text bg-dark text-white rounded-start-pill" id="addon-wrapping">Email</span>
               <input
                 type="email"
-                className="form-control bg-white"
+                className="form-control bg-white rounded-end-pill"
                 id="email"
                 name="email"
-                placeholder="Correo electrónico"
+                placeholder="example@mail.com"
                 required
               />
               <div className="invalid-feedback">Ingresa un correo válido.</div>
             </div>
 
-            <div className="mb-3">
+            <div className="mb-3 d-flex">
+              <span className="col-4 input-group-text bg-dark text-white rounded-start-pill" id="addon-wrapping">Password</span>
               <input
                 type="password"
-                className="form-control bg-white"
+                className="form-control bg-white rounded-end-pill"
                 id="password"
                 name="password"
                 placeholder="Contraseña"
