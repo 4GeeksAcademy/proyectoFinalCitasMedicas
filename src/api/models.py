ba5e31f4e21268bb5f6d199a1c62bad9697abda4
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-09-05 18:44:54 +0000

    Tabla User actualizada para Register :tada:

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] =mapped_column(String(80), nullable=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), unique=True, nullable=True)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -15,7 +17,9 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "phone": self.phone
             # do not serialize the password, its a security breach
         }
 
