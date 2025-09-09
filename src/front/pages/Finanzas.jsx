60c8c7398b78cf4c1b7aff6a28b8fda2a3af70b0
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-09-09 21:39:15 +0000

    Finanzas pages en proceso

--- src/front/pages/Finanzas.jsx
+++ src/front/pages/Finanzas.jsx
@@ -3,7 +3,7 @@ import { Navbar2 } from "../components/Navbar2"
 export const Finanzas = () => {
 
 
-    return(
+    return (
         <div
             style={{
                 backgroundColor: 'black',
@@ -14,6 +14,25 @@ export const Finanzas = () => {
         >
             <div className="d-flex">
                 <Navbar2 />
+                <div className="col bg-dark rounded-5 mx-3 ">
+                    <div className="text-white">
+                        <h1 className="text-white ms-4 mt-3"> Dashboard Financiero</h1>
+                        <p className="ms-5 mt-3">Análisis de ingresos y gestión de pagos</p>
+                    </div>
+                    <div className="d-flex justify-content-start mt-3 mx-5">
+                        <ul className="nav nav-tabs rounded-5 boder-2">
+                            <li className="nav-item ">
+                                <a className="nav-link rounded-5 text-white ntn btn-outline-dark" aria-current="page" href="#">Día</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link rounded-5 text-white" href="#">Semana</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link rounded-5 text-white" href="#">Mes</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
 
